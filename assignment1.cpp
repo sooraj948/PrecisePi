1a8a572eb36ba3a64e459dab5be4dbdc37943b05
chinmayparekh <chinmayparekh11@gmail.com>
2022-03-19 11:36:36 +0530

    modularized the code

--- assignment1.cpp
+++ assignment1.cpp
@@ -24,8 +24,8 @@ vector<int> addition(vector<int> a, vector<int> b)
 
         for (int i = l1; i < l2; i++)
         {
-            cout << "inside";
-            cout << "carry is " << carry << endl;
+            // cout << "inside";
+            // cout << "carry is " << carry << endl;
             c.push_back((b[i] + carry) % B);
             carry = (b[i] + carry) / B;
         }
@@ -37,7 +37,7 @@ vector<int> addition(vector<int> a, vector<int> b)
         {
             c.push_back((a[i] + b[i] + carry) % B);
             carry = (a[i] + b[i] + carry) / B;
-            cout << "carry is " << carry << endl;
+            // cout << "carry is " << carry << endl;
         }
 
         for (int i = l2; i < l1; i++)
@@ -69,6 +69,7 @@ int comparator(vector<int> a, vector<int> b) // returns 1 if a is greater than b
                 return 0;
         }
     }
+    return 0;
 }
 
 vector<int> adjustCarry(vector<int> a, int index) // returns a vector after adjusting the carry
@@ -85,13 +86,8 @@ vector<int> adjustCarry(vector<int> a, int index) // returns a vector after adju
             a[index] += B;
         }
     }
-    cout << "adjusted vector is" << endl;
-    for (int i = a.size() - 1; i >= 0; i--)
-    {
+    // cout << "adjusted vector is" << endl;
 
-        cout << a[i];
-    }
-    cout << endl;
     return a;
 }
 vector<int> adjustSign(vector<int> a)
@@ -130,8 +126,8 @@ vector<int> subtraction(vector<int> a, vector<int> b) // b-a
     int l1 = a.size(); // always less than l2
     int l2 = b.size();
     vector<int> c;
-    cout << "size of a is =" << l1 << endl;
-    cout << "size of b is =" << l2 << endl;
+    // cout << "size of a is =" << l1 << endl;
+    // cout << "size of b is =" << l2 << endl;
 
     int carry = 0;
     if (l1 == l2)
@@ -140,21 +136,21 @@ vector<int> subtraction(vector<int> a, vector<int> b) // b-a
         {
 
             c.push_back((-a[i] + b[i] + carry) % B);
-            cout << "i =" << i << " and c[i] = " << c[i] << endl;
+            // cout << "i =" << i << " and c[i] = " << c[i] << endl;
             carry = (-a[i] + b[i] + carry) / B;
-            cout << b[i] << "-" << a[i] << endl;
-            cout << "carry is " << carry << endl;
+            // cout << b[i] << "-" << a[i] << endl;
+            // cout << "carry is " << carry << endl;
         }
 
         for (int i = l1; i < l2; i++)
         {
 
-            cout << "doing from l1 to l2" << endl;
+            // cout << "doing from l1 to l2" << endl;
             c.push_back((b[i] + carry) % B);
-            cout << "i =" << i << " and c[i] = " << c[i] << endl;
+            // cout << "i =" << i << " and c[i] = " << c[i] << endl;
             carry = (b[i] + carry) / B;
 
-            cout << "carry is " << carry << endl;
+            // cout << "carry is " << carry << endl;
         }
 
         if (carry)
@@ -166,36 +162,36 @@ vector<int> subtraction(vector<int> a, vector<int> b) // b-a
         {
             if (a[i] < b[i])
             {
-                cout << "INSIDEEE" << endl;
+                // cout << "INSIDEEE" << endl;
                 c.push_back((-a[i] + b[i] + carry) % B);
-                cout << "i =" << i << " and c[i] = " << c[i] << endl;
+                // cout << "i =" << i << " and c[i] = " << c[i] << endl;
                 carry = (-a[i] + b[i] + carry) / B;
-                cout << b[i] << "-" << a[i] << endl;
-                cout << "carry is " << carry << endl;
+                // cout << b[i] << "-" << a[i] << endl;
+                // cout << "carry is " << carry << endl;
             }
             else
             {
 
-                cout << "i is less than l1-1" << endl;
+                // cout << "i is less than l1-1" << endl;
                 b = adjustCarry(b, i);
                 // b[i + 1] -= 1;
                 // b[i] += B;
                 c.push_back((-a[i] + b[i] + carry) % B);
-                cout << "i =" << i << " and c[i] = " << c[i] << endl;
+                // cout << "i =" << i << " and c[i] = " << c[i] << endl;
                 carry = (-a[i] + b[i] + carry) / B;
-                cout << b[i] << "-" << a[i] << endl;
-                cout << "carry is " << carry << endl;
+                // cout << b[i] << "-" << a[i] << endl;
+                // cout << "carry is " << carry << endl;
             }
         }
         for (int i = l1; i < l2; i++)
         {
 
-            cout << "doing from l1 to l2" << endl;
+            // cout << "doing from l1 to l2" << endl;
             c.push_back((b[i] + carry) % B);
-            cout << "i =" << i << " and c[i] = " << c[i] << endl;
+            // cout << "i =" << i << " and c[i] = " << c[i] << endl;
             carry = (b[i] + carry) / B;
 
-            cout << "carry is " << carry << endl;
+            // cout << "carry is " << carry << endl;
         }
 
         if (carry)
@@ -239,8 +235,8 @@ vector<int> multiplication(vector<int> a, vector<int> b)
 
             c[i + j] = temp % B;
             carry = temp / B;
-            print_number(c);
-            cout << "carry is " << carry << endl;
+            // print_number(c);
+            // cout << "carry is " << carry << endl;
         }
 
         c[i + l] = carry;
@@ -248,7 +244,40 @@ vector<int> multiplication(vector<int> a, vector<int> b)
 
     return c;
 }
+void checkAndSubtract(vector<int> a, vector<int> b)
+{
+    cout << "Subtract" << endl;
+    vector<int> d;
+    if (comparator(a, b)) // a is greater than b
+    {
+        // cout << "a-b" << endl;
+        d = subtraction(b, a); // d=a-b
+        d = adjustSign(d);
+    }
+    else
+    {
+        // cout << "b-a" << endl;
+        d = subtraction(a, b); // d=b-a
+        d = adjustNegativeSign(d);
+        // d[d.size() - 1] *= -1;
+    }
+    print_number(d);
+}
 
+void checkAndAdd(vector<int> a, vector<int> b)
+{
+    cout << "Adding" << endl;
+    vector<int> c = addition(a, b);
+    c = adjustSign(c);
+    print_number(c);
+}
+void checkAndMultiply(vector<int> a, vector<int> b)
+{
+    cout << "Multiplication" << endl;
+    vector<int> e = multiplication(a, b);
+    e = adjustSign(e);
+    print_number(e);
+}
 vector<int> karatsuba(vector<int> a, vector<int> b)
 {
     int l = a.size();
@@ -283,22 +312,6 @@ vector<int> karatsuba(vector<int> a, vector<int> b)
 
 int main()
 {
-    // string input;
-    // cin >> input;
-    // char ch;
-
-    // vector <int > input_vector;
-    // for (int i=0;i<input.size();i++)
-    // {
-    //     ch = input[i];
-    //     input_vector.push_back(atoi(&ch));
-    // }
-
-    // for(int i=0;i<input_vector.size();i++)
-    // {
-    //     cout<< input_vector.at(i);
-    // }
-    // cout <<endl;
 
     vector<int> a;
 
@@ -306,61 +319,16 @@ int main()
     // a.push_back(0);
     // a.push_back(0);
     a.push_back(7);
-    for (int i = a.size() - 1; i >= 0; i--)
-    {
-        cout << a[i];
-    }
-    cout << endl;
+
     vector<int> b;
     b.push_back(0);
     b.push_back(0);
     b.push_back(1);
-    for (int i = b.size() - 1; i >= 0; i--)
-    {
-        cout << b[i];
-    }
-    cout << endl;
-    cout << b.size() << endl;
+    print_number(a);
+    print_number(b);
 
-    vector<int> c = addition(a, b);
-    c = adjustSign(c);
-    cout << c.size() << endl;
-
-    for (int i = c.size() - 1; i > -1; i--)
-    {
-        cout << c[i] << "";
-    }
-    cout << endl;
-    vector<int> d;
-    if (comparator(a, b)) // a is greater than b
-    {
-        cout << "a-b" << endl;
-        d = subtraction(b, a); // d=a-b
-        d = adjustSign(d);
-    }
-    else
-    {
-        cout << "b-a" << endl;
-        d = subtraction(a, b); // d=b-a
-        d = adjustNegativeSign(d);
-        // d[d.size() - 1] *= -1;
-    }
-    // vector<int> d = subtraction(b, a);
-    cout << d.size() << endl;
-
-    for (int i = d.size() - 1; i > -1; i--)
-    {
-        cout << d[i] << "";
-    }
-    cout << endl;
-
-    cout << "Multiplication" << endl;
-    vector<int> e = multiplication(a, b);
-    e = adjustSign(e);
-    for (int i = e.size() - 1; i > -1; i--)
-    {
-        cout << e[i] << "";
-    }
-    cout << endl;
+    checkAndAdd(a, b);
+    checkAndSubtract(a, b);
+    checkAndMultiply(a, b);
     return 0;
 }
