d2150cd8b8d5ee6eba42e071a7674364a8bf6175
chinmayparekh <chinmayparekh11@gmail.com>
2022-03-29 23:27:15 +0530

    division

--- assignment1.cpp
+++ assignment1.cpp
@@ -18,26 +18,23 @@ void print_number(vector<int> d)
     cout << endl;
 }
 
-void print_pair_number(pair<vector<int>,int> d)
+void print_pair_number(pair<vector<int>, int> d)
 {
     // cout <<"size of vector in print pair "<< d.first.size()<<endl;
 
     // cout <<"0";
     int i;
-    for ( i= d.first.size() - 1; i > -d.second-1 ; i--)
+    for (i = d.first.size() - 1; i > -d.second - 1; i--)
     {
         cout << d.first[i] << "";
     }
-    cout<<".";
-    for(;i>-1;i--)
+    cout << ".";
+    for (; i > -1; i--)
     {
         cout << d.first[i] << "";
-
     }
-
 
     cout << endl;
-
 }
 vector<int> addition(vector<int> a, vector<int> b)
 {
@@ -123,6 +120,8 @@ pair<vector<int>, int> addition_real(vector<int> a, int power_a, vector<int> b, 
     // print_number(b);
 
     c = addition(a, b);
+    // cout << "Printing answer: " << endl;
+    // print_number(c);
 
     return make_pair(c, power_a);
 }
@@ -194,14 +193,14 @@ vector<int> adjustNegativeSign(vector<int> a)
     }
     return a;
 }
-vector<int> removeXZeroes(vector<int>a,int num)
+vector<int> removeXZeroes(vector<int> a, int num)
 {
     // cout <<"num is " << num <<endl;
     // print_number(a);
-    
+
     for (auto it = a.end() - 1; it != a.begin(); --it)
     {
-        if(num == 1)
+        if (num == 1)
         {
             // cout<<"Removed the zeroes"<<endl;
             // print_number(a);
@@ -212,11 +211,11 @@ vector<int> removeXZeroes(vector<int>a,int num)
         {
             a.erase(it);
         }
-       else
-       {
-        //    cout <<"inside Xzeroes and breaking"<< endl;
-           break;
-       }
+        else
+        {
+            //    cout <<"inside Xzeroes and breaking"<< endl;
+            break;
+        }
     }
     return a;
 }
@@ -480,9 +479,9 @@ vector<int> division(vector<int> a, vector<int> b)
 
     r.push_back(0);
     // cout << r.size() << endl;
-    //finding first non zero
+    // finding first non zero
 
-    int denom=1;
+    int denom = 1;
     // for (int i=l-1;i>-1;i--)
     // {
     //     if (b[i])
@@ -491,12 +490,10 @@ vector<int> division(vector<int> a, vector<int> b)
     //         break;
     //     }
 
-            
-
     // }
     for (int i = k - l; i >= 0; i--)
     {
-        q[i] = ((r[i + l] * B) + r[i + l - 1]) / b[l-1];
+        q[i] = ((r[i + l] * B) + r[i + l - 1]) / b[l - 1];
         // q[i] += r[i + l - 1];
         // q[i] = q[i] / b[l - 1];
 
@@ -548,10 +545,10 @@ vector<int> pad_zeroes_at_end(vector<int> a)
     // if (a.size()<precision)
     // {
 
-        for (int i = 0; i < count*precision; i++)
-        {
-            result.push_back(0);
-        }
+    for (int i = 0; i < count * precision; i++)
+    {
+        result.push_back(0);
+    }
     // }
     for (int i = 0; i < a.size(); i++)
     {
@@ -559,7 +556,7 @@ vector<int> pad_zeroes_at_end(vector<int> a)
     }
     return result;
 }
-vector<int> removeTrailingZeroes(vector<int>a)
+vector<int> removeTrailingZeroes(vector<int> a)
 {
     for (auto it = a.begin(); it != a.end(); ++it)
     {
@@ -569,11 +566,11 @@ vector<int> removeTrailingZeroes(vector<int>a)
             // ing removed"<<endl;
             a.erase(it);
         }
-       else
-       {
-        //    cout<<"Breaking"<<endl;
-           break;
-       }
+        else
+        {
+            //    cout<<"Breaking"<<endl;
+            break;
+        }
     }
     // cout<<"Trailing zeroes have been removed"<<endl;
     // print_number(a);
@@ -589,12 +586,12 @@ pair<vector<int>, int> division_real(vector<int> a, int power_a, vector<int> b, 
     // print_number(c);
     // cout <<" c is "<<endl;
     // print_number(c);
-    b= removeXZeroes(b,b.size()+power_b);
+    b = removeXZeroes(b, b.size() + power_b);
     // b = removeTrailingZeroes(b);
-    
+
     c = division(c, b);
-   //********the power is the problem*********
-    return make_pair(c, power_a - power_b - count*precision);
+    //********the power is the problem*********
+    return make_pair(c, power_a - power_b - count * precision);
 }
 // pair<vector<int>, vector<int>> squareroot(vector<int> R, int power_R, vector<int> x0, int power_x0)
 // {
@@ -615,14 +612,14 @@ pair<vector<int>, int> division_real(vector<int> a, int power_a, vector<int> b, 
 // }
 pair<vector<int>, int> sq_root(vector<int> R, int power_R, vector<int> x0, int power_x0)
 {
-    static int count=0;
+    static int count = 0;
     count++;
-    cout << "count is "<<count<<endl;
+    cout << "count is " << count << endl;
     pair<vector<int>, int> x;
 
     vector<int> half;
-    cout<<"Pushing"<<endl;
-    half.push_back(B/2); //*10**-1
+    cout << "Pushing" << endl;
+    half.push_back(B / 2); //*10**-1
     half.push_back(0);
 
     pair<vector<int>, int> temp; // inside ()
@@ -631,40 +628,37 @@ pair<vector<int>, int> sq_root(vector<int> R, int power_R, vector<int> x0, int p
 
     // R_by_x.push_back(0);
 
-    cout << "R by x is "<<endl;
-    //R_by_x is empty ??? check this
-    // print_number(R_by_x.first);
+    cout << "R by x is " << endl;
+    // R_by_x is empty ??? check this
+    //  print_number(R_by_x.first);
     print_pair_number(R_by_x);
 
-
     temp = addition_real(x0, power_x0, R_by_x.first, R_by_x.second);
-    cout <<"temp ";
+    cout << "temp ";
     print_pair_number(temp);
 
     x = multiplication_real(half, -1, temp.first, temp.second);
 
-    cout <<"intermediate x is "<<endl;
+    cout << "intermediate x is " << endl;
 
     // print_number(x.first);
     // cout <<x.second<<endl;
     print_pair_number(x);
 
-
-    if (count<12) // some arbitrary precision
+    if (count < 12) // some arbitrary precision
         return sq_root(R, power_R, x.first, x.second);
     else
         return x;
 }
 
-vector <int> roundoff(vector<int> a,int n)
+pair<vector<int>, int> roundoff(pair<vector<int>, int> a, int n)
 {
-    vector<int> ans;
+    pair<vector<int>, int> ans;
 
-    for (int i=a.size()-n;i<a.size();i++)
-        ans.push_back(a[i]);
-
+    for (int i = a.first.size() - n; i < a.first.size(); i++)
+        ans.first.push_back(a.first[i]);
+    ans.second = a.first.size() - n + a.second;
     return ans;
-
 }
 
 pair<vector<int>, int> squareroot(vector<int> R, int power_R, vector<int> x0, int power_x0)
@@ -675,16 +669,16 @@ pair<vector<int>, int> squareroot(vector<int> R, int power_R, vector<int> x0, in
     int count = 0;
 
     vector<int> half;
-    half.push_back(B/2);
+    half.push_back(B / 2);
 
     pair<vector<int>, int> answer;
     while (count < precision)
-    { 
+    {
         // if(count==12)
-    // {
-    //     break;
-    // }   
-    // x0 =removeTrailingZeroes(x0);
+        // {
+        //     break;
+        // }
+        // x0 =removeTrailingZeroes(x0);
 
         pair<vector<int>, int> R_by_Xn;
         // cout << "R = "; // 2
@@ -708,95 +702,246 @@ pair<vector<int>, int> squareroot(vector<int> R, int power_R, vector<int> x0, in
         // cout << "Power of answer = " << answer.second << endl;
         x0 = answer.first;
         power_x0 = answer.second;
-       
-
 
         // x0 = roundoff(x0,50);
-        
-
 
         count++;
     }
     return answer;
 }
 
-pair<vector<int> , int> pi()
+pair<vector<int>, int> pi()
 {
-    //Constants
-    vector<int> two ;
+    // Constants
+    vector<int> two; // 2
     two.push_back(2);
 
     vector<int> b;
-    b.push_back(4);
+    b.push_back(4); // 1.4
     b.push_back(1);
+    pair<vector<int>, int> one;
+    one.first = {1};
+    one.second = 0;
+    // vector<int> one; // 1
+    // one.push_back(1);
 
-     vector<int> one ;
-    one.push_back(1);
+    // vector<int> half; // 5
+    // half.push_back(B / 2);
 
-    vector<int> half ;
-    one.push_back(B/2);
-
-    //initializing a0,b0,p0
-    pair<vector<int>,int> a0 = squareroot(two,0,b,-1) ;//2**0.5
-
-    pair<vector<int> ,int> b0;
-    b0.first.push_back(0);
+    // initializing a0,b0,p0
+    pair<vector<int>, int> a0 = squareroot(two, 0, b, -1); // 2**0.5
+    // print_pair_number(a0);
+    pair<vector<int>, int> b0;
+    b0.first = {0};
     b0.second = 0;
+    pair<vector<int>, int> five;
+    five.first = {5};
+    five.second = -1;
 
-    pair<vector<int> ,int> p0 = addition_real(two,0,a0.first,0);
+    pair<vector<int>, int> p0 = addition_real(two, 0, a0.first, a0.second);
+    // cout << "Printing p0" << endl;
+    // print_pair_number(p0);
+    int count = 0;
 
-    int count =0;
-
-   
-
-    
-    while(count<6)
+    while (count < 3)
     {
-        //bn-> an->pn
-        
+        // bn-> an->pn
 
-        pair<vector<int> ,int> one_plus_bn = addition_real(b0.first,b0.second,one,0);
-        pair<vector<int> ,int> root_an = squareroot(a0.first,a0.second,b,-1);
-        pair<vector<int> ,int> an_plus_bn = addition_real(b0.first,b0.second,a0.first,a0.second);
-        pair<vector<int> ,int> numerator_bn1 = multiplication_real(one_plus_bn.first,one_plus_bn.second,root_an.first,root_an.second);
-        b0 = division_real(numerator_bn1.first,numerator_bn1.second,an_plus_bn.first,an_plus_bn.second);
-
-        pair<vector<int> ,int> one_by_root_an = division_real(one,0,root_an.first,root_an.second);
-        pair<vector<int> ,int> numerator_an1 = addition_real(root_an.first,root_an.second,one_by_root_an.first,one_by_root_an.second);
-        a0 = multiplication_real(half,-1,numerator_an1.first,numerator_an1.second);
-
-        pair<vector<int> ,int> one_plus_an1 = addition_real(one,0,a0.first,a0.second);
-        pair<vector<int> ,int> pn_times_bn1 = multiplication_real(p0.first,p0.second,b0.first,b0.second);
-        pair<vector<int> ,int> numerator_pn1 = multiplication_real(one_plus_an1.first,one_plus_an1.second,pn_times_bn1.first,pn_times_bn1.second);
-        pair<vector<int> ,int> one_plus_bn1 = addition_real(one,0,b0.first,b0.second);
-        p0 = division_real(numerator_pn1.first,numerator_pn1.second,one_plus_bn1.first,one_plus_bn1.second);
-        
-    }    
-
+        pair<vector<int>, int> one_plus_bn = addition_real(b0.first, b0.second, one.first, one.second); // 1+b
+        // print_pair_number(one_plus_bn);
+        pair<vector<int>, int> root_an = squareroot(a0.first, a0.second, b, -1);
+        // print_pair_number(root_an);
+        pair<vector<int>, int> an_plus_bn = addition_real(b0.first, b0.second, a0.first, a0.second);
+        // print_pair_number(an_plus_bn);
+        pair<vector<int>, int> numerator_bn1 = multiplication_real(one_plus_bn.first, one_plus_bn.second, root_an.first, root_an.second);
+        // print_pair_number(numerator_bn1);
+        b0 = division_real(numerator_bn1.first, numerator_bn1.second, an_plus_bn.first, an_plus_bn.second);
+        // print_pair_number(b0);
+        pair<vector<int>, int> one_by_root_an = division_real(one.first, one.second, root_an.first, root_an.second);
+        // print_pair_number(one_by_root_an);
+        pair<vector<int>, int> numerator_an1 = addition_real(root_an.first, root_an.second, one_by_root_an.first, one_by_root_an.second);
+        // print_pair_number(numerator_an1);
+        a0 = multiplication_real(five.first, five.second, numerator_an1.first, numerator_an1.second);
+        // print_pair_number(a0);
+        pair<vector<int>, int> one_plus_an1 = addition_real(one.first, one.second, a0.first, a0.second);
+        // print_pair_number(one_plus_an1);
+        pair<vector<int>, int> pn_times_bn1 = multiplication_real(p0.first, p0.second, b0.first, b0.second);
+        // cout << "************************" << endl;
+        // print_pair_number(pn_times_bn1);
+        pair<vector<int>, int> numerator_pn1 = multiplication_real(one_plus_an1.first, one_plus_an1.second, pn_times_bn1.first, pn_times_bn1.second);
+        // print_pair_number(numerator_pn1);
+        pair<vector<int>, int> one_plus_bn1 = addition_real(one.first, one.second, b0.first, b0.second);
+        // print_pair_number(one_plus_bn1);
+        p0 = division_real(numerator_pn1.first, numerator_pn1.second, one_plus_bn1.first, one_plus_bn1.second);
+        p0 = roundoff(p0, 256);
+        print_pair_number(p0);
+        count++;
+    }
 
     return p0;
-
-    
-    
 }
+
+// pair<vector<int>, vector<int>> PI()
+// {
+//     vector<int> num1, num2, dec1, dec2;
+//     num1.push_back(2);
+//     num2.push_back(1);
+//     dec2.push_back(5);
+//     pair<vector<int>, vector<int>> a = squareroot(num1, num2, dec1, dec2);
+//     vector<int> a1;
+
+//     // for(int i=0;i<a.first.size();i++) cout<<a.first[i]<<" ";
+//     // cout<<endl;
+//     // for(int i=0;i<a.second.size();i++) cout<<a.second[i]<<" ";
+//     // cout<<endl;
+
+//     pair<vector<int>, vector<int>> b;
+//     pair<vector<int>, vector<int>> p = Add_with_dec(a.first, a.second, num1, dec1);
+
+//     // for(int i=0;i<p.first.size();i++) cout<<p.first[i]<<" ";
+//     // cout<<endl;
+//     // for(int i=0;i<p.second.size();i++) cout<<p.second[i]<<" ";
+//     // cout<<endl;
+
+//     int j = 0;
+//     while (j < 8)
+//     {
+//         j++;
+
+//         pair<vector<int>, vector<int>> tempa = a;
+//         pair<vector<int>, vector<int>> ra;
+//         pair<vector<int>, vector<int>> ra1;
+
+//         // for(int i=0;i<40;i++) a1.push_back(tempa.second[i]);
+//         ra = squareroot(a.first, num1, a.second, dec1);
+
+//         // for(int i=0;i<ra.first.size();i++) cout<<ra.first[i]<<" ";
+//         // cout<<endl;
+//         // for(int i=0;i<ra.second.size();i++) cout<<ra.second[i]<<" ";
+//         // cout<<endl;
+//         // cout<<endl;
+
+//         // vector<int> tra;
+//         // for(int i=0;i<20;i++)
+//         // {
+//         //     tra.push_back(ra.second[i]);
+//         // }
+//         ra1 = Div_with_dec(num2, dec1, ra.first, ra.second);
+
+//         // for(int i=0;i<ra1.first.size();i++) cout<<ra1.first[i]<<" ";
+//         // cout<<endl;
+//         // for(int i=0;i<ra1.second.size();i++) cout<<ra1.second[i]<<" ";
+//         // cout<<endl;
+//         // cout<<endl;
+
+//         a = Add_with_dec(ra.first, ra.second, ra1.first, ra1.second);
+//         a = Div_with_dec(a.first, a.second, num1, dec1);
+//         // for(int i=0;i<a.first.size();i++) cout<<a.first[i]<<" ";
+//         // cout<<endl;
+//         // for(int i=0;i<a.second.size();i++) cout<<a.second[i]<<" ";
+//         // cout<<endl;
+//         // cout<<endl;
+
+//         pair<vector<int>, vector<int>> tempb = b;
+//         pair<vector<int>, vector<int>> b1;
+//         pair<vector<int>, vector<int>> addab;
+//         pair<vector<int>, vector<int>> b1rta;
+
+//         b1 = Add_with_dec(b.first, b.second, num2, dec1);
+
+//         // for(int i=0;i<b1.first.size();i++) cout<<b1.first[i]<<" ";
+//         // cout<<endl;
+//         // for(int i=0;i<b1.second.size();i++) cout<<b1.second[i]<<" ";
+//         // cout<<endl;
+
+//         addab = Add_with_dec(tempb.first, tempb.second, tempa.first, tempa.second);
+
+//         // for(int i=0;i<addab.first.size();i++) cout<<addab.first[i]<<" ";
+//         // cout<<endl;
+//         // for(int i=0;i<addab.second.size();i++) cout<<addab.second[i]<<" ";
+//         // cout<<endl;
+
+//         b1rta = Mul_with_dec(b1.first, b1.second, ra.first, ra.second);
+
+//         // for(int i=0;i<b1rta.first.size();i++) cout<<b1rta.first[i]<<" ";
+//         // cout<<endl;
+//         // for(int i=0;i<b1rta.second.size();i++) cout<<b1rta.second[i]<<" ";
+//         // cout<<endl;
+//         // cout << endl;
+
+//         b = Div_with_dec(b1rta.first, b1rta.second, addab.first, addab.second);
+//         // cout << "b"<<endl;
+//         // for(int i=0;i<b.first.size();i++) cout<<b.first[i]<<" ";
+//         // cout<<endl;
+//         // for(int i=0;i<b.second.size();i++) cout<<b.second[i]<<" ";
+//         // cout<<endl;
+//         // cout<<endl;
+
+//         pair<vector<int>, vector<int>> tempp = p;
+//         pair<vector<int>, vector<int>> a11;
+//         pair<vector<int>, vector<int>> b11;
+//         pair<vector<int>, vector<int>> a11p;
+//         pair<vector<int>, vector<int>> a11pb;
+
+//         b11 = Add_with_dec(b.first, b.second, num2, dec1);
+//         //     // for(int i=0;i<b11.first.size();i++) cout<<b11.first[i]<<" ";
+//         //     // cout<<endl;
+//         //     // for(int i=0;i<b11.second.size();i++) cout<<b11.second[i]<<" ";
+//         //     // cout<<endl;
+
+//         a11 = Add_with_dec(a.first, a.second, num2, dec1);
+//         //     // for(int i=0;i<a11.first.size();i++) cout<<a11.first[i]<<" ";
+//         //     // cout<<endl;
+//         //     // for(int i=0;i<a11.second.size();i++) cout<<a11.second[i]<<" ";
+//         //     // cout<<endl;
+
+//         a11p = Mul_with_dec(a11.first, a11.second, tempp.first, tempp.second);
+//         //     // for(int i=0;i<a11p.first.size();i++) cout<<a11p.first[i]<<" ";
+//         //     // cout<<endl;
+//         //     // for(int i=0;i<a11p.second.size();i++) cout<<a11p.second[i]<<" ";
+//         //     // cout<<endl;
+
+//         a11pb = Mul_with_dec(a11p.first, a11p.second, b.first, b.second);
+//         //     // for(int i=0;i<a11pb.first.size();i++) cout<<a11pb.first[i]<<" ";
+//         //     // cout<<endl;
+//         //     // for(int i=0;i<a11pb.second.size();i++) cout<<a11pb.second[i]<<" ";
+//         //     // cout<<endl;
+
+//         // vector<int> tb11;
+//         // for(int i=0;i<20;i++)
+//         // {
+//         //     tb11.push_back(b11.second[i]);
+//         // }
+//         p = Div_with_dec(a11pb.first, a11pb.second, b11.first, b11.second);
+//     }
+//     for (int i = 0; i < p.first.size(); i++)
+//         cout << p.first[i] << " ";
+//     cout << endl;
+//     for (int i = 0; i < p.second.size(); i++)
+//         cout << p.second[i] << " ";
+//     cout << endl;
+//     return a;
+// }
 
 int main()
 {
+    vector<int> one;
+    one.push_back(1);
 
     vector<int> a;
 
-    
-    a.push_back(2);
-   
+    a = {4, 1, 4, 1};
+    // a.push_back(5);
+
     vector<int> b;
-    
+
     b.push_back(4);
     b.push_back(1);
-    
+
     print_number(a);
     print_number(b);
 
-    // checkAndAdd(a, b);
+    checkAndAdd({0}, {1});
     // checkAndSubtract(a, b);
     // checkAndMultiply(a, b);
     // vector<int> q = division(a, b);
@@ -808,10 +953,10 @@ int main()
     // // trialSubtraction(a, b);
     // // trialAddition(a, b);
 
-    // printf("Addition real \n");
-    // pair<vector<int>, int> c = addition_real(a, 0, b, -2);
-    // print_number(c.first);
-    // printf("%d\n", c.second);
+    printf("Addition real \n");
+    pair<vector<int>, int> c = addition_real({0}, 0, {1}, 0);
+    print_number(c.first);
+    printf("%d\n", c.second);
 
     // printf("Subtraction real \n");
     // pair<vector<int>, int> c1 = subtraction_real(a, 0, b, -2); // 2-0.01=1.99
@@ -828,22 +973,24 @@ int main()
     // printf("Division real \n");
     // pair<vector<int>, int> c3 = division_real(a, 0, b, -5);cout<<"Answer = "<<endl;
     // print_number(c3.first);
-    
+
     // printf("%d\n", c3.second);
 
     // print_pair_number(c3);
 
     // printf("Sq root  \n");
-    // pair<vector<int>, int> c4 = squareroot(a, 0, b, -1);
+    // pair<vector<int>, int> c4 = squareroot(a, -3, b, -1);
     // // print_number(c4.first);
     // // printf("%d\n", c4.second);
 
     // print_pair_number(c4);
+    // pair<vector<int>, int> reciprocal = division_real(one, 0, c4.first, c4.second);
+    // // pair<vector<int>, int> c5 = squareroot(reciprocal.first, reciprocal.second, b, -1);
+    // print_pair_number(reciprocal);
 
-    cout <<"PI:"<< endl;
+    cout << "PI:" << endl;
     pair<vector<int>, int> PI = pi();
     print_pair_number(PI);
 
     return 0;
 }
-
